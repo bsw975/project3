85e6ec94e4182372a8f467627164c5ca6c4b60b1
unknown <saif_dar@outlook.com>
2018-08-21 16:54:40 -0700

    frontend authentication

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+// Home.js
+
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Home Component</h1>
+            </div>
+        );
+    }
+}
